f66bc78d44e7686d5c52b663c95a5743ca38308f
Eric <neo_eric@hotmail.com>
2014-07-30 00:59:50 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,116 @@
+var endPoint = "http://api.mtgdb.info/";
+
+$(document).ready(function() 
+{
+	$header = $("#header");
+	$preview = $("#preview");
+	$content = $("#content");
+	if (getParameterByName("set") != null)
+		loadCardsList(getParameterByName("set"));
+	else
+		loadSetsList();
+});
+
+function loadCardsList(setID)
+{
+	$header.html('Loading cards for ' + setID);
+	
+	var url = endPoint + "sets/" + setID + "/cards/?fields=id,name,colors,cardSetName";
+	var request = $.get(url);
+	request.done(function(data)
+	{
+		$content.html('');
+		var templateSource = $("#cardListItemTemplate").html();
+		var template = Handlebars.compile(templateSource);
+		
+		for (i = 0; i < data.length; i++) 
+		{
+			var card = data[i];
+			
+			if (i == 0)
+				$header.html(card.cardSetName);
+			
+			var cardData = {};
+			cardData.name = card.name;
+			cardData.id = card.id;
+			
+			var html = template(cardData);
+			$content.append(html);
+		}
+	});
+}
+
+function loadSetsList()
+{
+	$header.html('Loading character data...');
+	
+	var url = endPoint + "sets/";
+	var request = $.get(url);
+	request.done(function(data)
+	{
+		$header.html('Done. Loaded ' + data.length + ' sets.');
+		$content.html('');
+		var templateSource = $("#setListItemTemplate").html();
+		var template = Handlebars.compile(templateSource);
+		
+		var setsList = data.reverse();
+		var promoList = [];
+		for (i = 0; i < setsList.length; i++) 
+		{ 
+			var set = setsList[i];
+			if (set.type == "Online")
+			{
+				continue;
+			}
+			else if (set.type != "Expansion" && set.type != "Core")
+			{	
+				promoList.push(set);
+				continue;
+			}
+			
+			var setData = parseSetData(set);
+			var html = template(setData);
+			$content.append(html);
+		}
+		
+		for (i = 0; i < promoList.length; i++) 
+		{ 
+			var setData = parseSetData(promoList[i]);
+			var html = template(setData);
+			$content.append(html);
+		}
+	}); 
+}
+
+function parseSetData(set)
+{
+	var setData = {};
+	setData.name = set.name;
+	setData.link = '?set=' + set.id;
+	
+	return setData;
+}
+
+function getParameterByName(name)
+{
+	name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+	var regexS = "[\\?&]"+name+"=([^&#]*)";
+	var regex = new RegExp( regexS );
+	var results = regex.exec( window.location.href );
+	if( results == null )
+		return null;
+	else
+		return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function openPreview(img)
+{
+	$preview.offset({ top: $( document ).scrollTop() });
+	$preview.attr("src", "http://api.mtgdb.info/content/card_images/" + img.id + ".jpeg");
+	$preview.show();
+}
+
+function closePreview(img)
+{
+	$preview.hide();
+}
